6a6dde157aca68cea5f12450ef0fc99867d8e7e9
Neli Kachan <nellikcn@gmail.com>
2026-03-27 12:32:57 +0100

    fix service controller and add index.html

---
+++ src/main/java/com/example/grooming_booking/controller/ServiceController.java
@@ -0,0 +1,23 @@
+package com.example.grooming_booking.controller;
+
+import com.example.grooming_booking.entity.GroomingService;
+import com.example.grooming_booking.repository.ServiceRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/services")
+public class ServiceController {
+
+    private final ServiceRepository serviceRepository;
+
+    public ServiceController(ServiceRepository serviceRepository) {
+        this.serviceRepository = serviceRepository;
+    }
+
+    @GetMapping
+    public List<GroomingService> getAll() {
+        return serviceRepository.findAll();
+    }
+}
